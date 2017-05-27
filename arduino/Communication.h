cf10811bdf78d6282c9c06e6649425013aacc2b0
dutchwilliam <w.j.vegt@gmail.com>
2017-05-27 17:40:19 +0200

    Accuracy upgrade

---
+++ arduino/Communication.h
@@ -0,0 +1,15 @@
+
+#ifndef Communication_h
+#define Communication_h
+
+#include "Arduino.h"
+
+class Communication
+{
+  public:
+    Communication();
+    String readInput();
+};
+
+#endif
+
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -23,6 +23,10 @@ int xLoc = 0;
 boolean xDone;
 boolean dirX;
 int speedX;
+int onXBlackC;
+int brakeSpeed;
+boolean split;
+int splitC;
 
 boolean onYWhite;
 int onYWhiteC;
@@ -33,6 +37,7 @@ int yLoc = 0;
 boolean yDone;
 boolean dirY;
 int speedY;
+int onYBlackC;
 
 int iLoc;
 boolean onIWhite;
@@ -40,6 +45,7 @@ boolean onILoc;
 boolean iDone;
 int speedI;
 int onIWhiteC;
+int onIBlackC;
 
 void setup() {
   //  while(true){};
@@ -51,79 +57,67 @@ void setup() {
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
   Serial.begin(9600);
-
-  //  while(true){
-  //  while(!yDone||!xDone){
-  //    moveYCoor(1);
-  //    moveXCoor(2);
-  //    delay(5);
-  //  }
-  //  extractPackage();
-  //  resetVars();
-  //  while(!yDone||!xDone){
-  //    moveYCoor(2);
-  //    moveXCoor(1);
-  //    delay(5);
-  //  }
-  //  extractPackage();
-  //  resetVars();
-  //  while(!yDone||!xDone){
-  //    moveYCoor(3);
-  //    moveXCoor(2);
-  //    delay(5);
-  //  }
-  //  extractPackage();
-  //  resetVars();
-  //  while(!yDone||!xDone){
-  //    moveYCoor(4);
-  //    moveXCoor(1);
-  //    delay(5);
-  //  }
-  //  extractPackage();
-  //  resetVars();
-  //  while(!yDone||!xDone){
-  //    moveYCoor(5);
-  //    moveXCoor(2);
-  //    delay(5);
-  //  }
-  //  extractPackage();
-  //  resetVars();
-  //  while(!yDone||!xDone){
-  //    moveYCoor(1);
-  //    moveXCoor(1);
-  //    delay(5);
-  //  }
-  //}
+  moveYMotor(100,0);
+  moveXMotor(110,0);
+  delay(3000);
+    
+  while(true){
+    while(!yDone||!xDone){
+      moveYCoor(1);
+      moveXCoor(1);
+    }
+    extractPackage();
+    resetVars();
+    while(!yDone||!xDone){
+      moveYCoor(2);
+      moveXCoor(2);
+    }
+    extractPackage();
+    resetVars();
+    while(!yDone||!xDone){
+      moveYCoor(3);
+      moveXCoor(3);
+    }
+    extractPackage();
+    resetVars();
+    while(!yDone||!xDone){
+      moveYCoor(4);
+      moveXCoor(4);
+    }
+    extractPackage();
+    resetVars();
+    while(!yDone||!xDone){
+      moveYCoor(5);
+      moveXCoor(5);
+    }
+    extractPackage();
+    resetVars();
+  }
 
   moveIMotor(80, 0);
   delay(3000);
   while (!iDone) {
     emptyPackage(2);
-    delay(5);
     Serial.println(iLoc);
   }
   resetVars();
   while (!iDone) {
     emptyPackage(3);
-    delay(5);
     Serial.println(iLoc);
   }
   resetVars();
   while (!iDone) {
     emptyPackage(4);
-    delay(5);
     Serial.println(iLoc);
   }
   resetVars();
   while (!iDone) {
     emptyPackage(5);
-    delay(5);
     Serial.println(iLoc);
   }
   resetVars();
   while (!iDone) {
     emptyPackage(6);
-    delay(5);
     Serial.println(iLoc);
   }
   resetVars();
@@ -135,19 +129,17 @@ void loop() {
 
 void moveXCoor(int coor) {
   int trainSensor = readSensor(0, 200);
-  int brakeSpeed;
-  speedX = 100;
+  
+  speedX = 105;
   if (startXLoc == 0) {
     startXLoc = 1;
     if (coor - xLoc > 0) {
       dirX = true;
       moveXMotor(speedX, dirX);
-      delay(100);
       brakeSpeed = coor - xLoc;
     } else {
       dirX = false;
       moveXMotor(speedX, dirX);
-      delay(100);
       brakeSpeed = xLoc - coor;
     }
   }
@@ -155,6 +147,7 @@ void moveXCoor(int coor) {
 
   if (trainSensor && !onXWhite && !onXLoc) { //Sensor Wit & Niet op wit & niet al op locatie
     onXWhite = true;
+    onXBlackC=0;
     if (dirX) {
       xLoc++; //Tel op of af aan de hand van richting.
     } else {
@@ -171,18 +164,34 @@ void moveXCoor(int coor) {
   } else if (trainSensor && onXLoc) { //Sensor Wit & op locatie
     if (onXWhiteC < 1) {
       moveXMotor(255, !dirX);
-      delay(brakeSpeed);
+      delay(brakeSpeed*10);
     }
-    moveXMotor(0, !dirX);
+    moveXMotor(0, dirX);
     onXWhite = true;
+    onXBlackC=0;
     onXWhiteC++;
   } else if (!trainSensor && onXLoc) { //Sensor zwart && op locatie
-    moveXMotor(speedX - 15, !dirX);
-  } else if (!trainSensor) { // Sensor zwart
+    if(split){
+      moveXMotor(speedX+20, !dirX);
+      splitC++;
+      if(splitC>200){
+        split=false;
+      }
+    }else{
+      moveXMotor(0, !dirX);
+      splitC++;
+      if(splitC>400){
+        splitC=0;
+        split=true;
+      }
+    }
+  } else if (!trainSensor&&onXBlackC>200) { // Sensor zwart
     onXWhite = false;
     moveXMotor(speedX, dirX);
+  }else if (!trainSensor) { // Sensor zwart
+    onXBlackC++;
   }
-  if (onXWhiteC == 500) {
+  if (onXWhiteC == 1000) {
     xDone = true;
     onXWhite = true;
   }
@@ -205,6 +214,7 @@ void moveYCoor(int coor) {
 
   if (rackSensor && !onYWhite && !onYLoc) { //Sensor Wit & Niet op wit & niet al op locatie
     onYWhite = true;
+    onYBlackC=0;
     if (dirY) {
       yLoc++; //Tel op of af aan de hand van richting.
     } else {
@@ -221,12 +231,15 @@ void moveYCoor(int coor) {
   } else if (rackSensor && onYLoc) { //Sensor Wit & op locatie
     moveYMotor(0, !dirY);
     onYWhite = true;
+    onYBlackC=0;
     onYWhiteC++;
   } else if (!rackSensor && onYLoc && !dirY) {
     moveYMotor(100, !dirY);
-  } else if (!rackSensor) { // Sensor zwart
+  } else if (!rackSensor&&onYBlackC>20) { // Sensor zwart
     onYWhite = false;
-    moveYMotor(speedY, dirY);
+    moveYMotor(speedY, dirX);
+  }else if (!rackSensor) { // Sensor zwart
+    onYBlackC++;
   }
   if (onYWhiteC == 500) {
     yDone = true;
@@ -235,7 +248,7 @@ void moveYCoor(int coor) {
 
 void extractPackage() {
   moveIMotor(70, 0);
-  delay(2500);
+  delay(1200);
   moveIMotor(0, 0);
 
   while (readSensor(4, 200)) {
@@ -255,13 +268,12 @@ void extractPackage() {
   }
   moveYMotor(0, 0);
 }
-
 void emptyPackage(int coor) {
   int insertSensor = readSensor(5, 600);
-  speedI = 60;
+  speedI = 80;
   if (insertSensor && !onIWhite && !onILoc) { //Sensor Wit & Niet op wit & niet al op locatie
     onIWhite = true;
-    //    Serial.println(1);
+    onIBlackC=0;
     iLoc++;
     if (iLoc != coor) { // nog niet op locatie
       moveIMotor(speedI, 1);
@@ -271,18 +283,16 @@ void emptyPackage(int coor) {
     }
   } else if (insertSensor && onIWhite && !onILoc) { // Sensor wit & op wit & niet op locatie
     moveIMotor(speedI, 1);
-    //    Serial.println(2);
   } else if (insertSensor && onILoc) { //Sensor Wit & op locatie
     moveIMotor(0, 0);
-    //    Serial.println(3);
     onIWhiteC++;
-  } else if (!insertSensor) { // Sensor zwart
+  }else if (!insertSensor&&onIBlackC>20) { // Sensor zwart
     onIWhite = false;
-    //    Serial.println(4);
     moveIMotor(speedI, 1);
+  }else if (!insertSensor) { // Sensor zwart
+    onIBlackC++;
   }
   if (onIWhiteC == 300) {
-    //    Serial.println(5);
     iDone = true;
   }
 }
@@ -315,14 +325,17 @@ void resetVars() {
   onXLoc = false;
   onXWhiteC = 0;
   startXLoc = 0;
+  onXBlackC=0;
 
   yDone = false;
   onYLoc = false;
   onYWhiteC = 0;
   startYLoc = 0;
+  onYBlackC=0;
 
   iDone = false;
   onILoc = false;
   onIWhiteC = 0;
+  onIBlackC=0;
 }
 
